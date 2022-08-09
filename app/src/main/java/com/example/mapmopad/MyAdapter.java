23da128594dbfaeee8da1732749ebc71c0dd4b44
wodus <wodustjdus34@gmail.com>
2022-08-09 14:41:49 +0900

    endless recyclerview 코드 추가, homebtn 추가 (작동 X)

--- app/src/main/java/com/example/mapmopad/MyAdapter.java
+++ app/src/main/java/com/example/mapmopad/MyAdapter.java
@@ -6,6 +6,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,10 +16,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     Context context;
     RealmResults<Note> notesList;
+
+    private final int VIEW_TYPE_NOTE = 0;
+    private final int VIEW_TYPE_LOADING = 1;
 
     public MyAdapter(Context context, RealmResults<Note> notesList) {
         this.context = context;
@@ -27,53 +31,91 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view, parent, false));
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_NOTE) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view, parent, false);
+            return new ItemViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
+            return new LoadingViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        Note note = notesList.get(position);
-        holder.descriptionOutput.setText(note.getDescription());
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+            populateItemRows((ItemViewHolder) holder, position);
+        } else if (holder instanceof LoadingViewHolder) {
+            showLoadingView((LoadingViewHolder) holder, position);
+        }
 
-        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
+        //삭제
+//        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//
+//                PopupMenu menu = new PopupMenu(context, v);
+//                menu.getMenu().add("Delete");
+//                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+//                    @Override
+//                    public boolean onMenuItemClick(MenuItem item) {
+//                        if(item.getTitle().equals("Delete")) {
+//                            Realm realm = Realm.getDefaultInstance();
+//                            realm.beginTransaction();
+//                            note.deleteFromRealm();
+//                            realm.commitTransaction();
+//                            Toast.makeText(context, "Note deleted", Toast.LENGTH_SHORT).show();
+//                        }
+//                        return true;
+//                    }
+//                });
+//                menu.show();
+//
+//                return true;
+//            }
+//        });
+    }
 
-                PopupMenu menu = new PopupMenu(context, v);
-                menu.getMenu().add("DELETE");
-                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        if (item.getTitle().equals("DELETE")) {
-                            Realm realm = Realm.getDefaultInstance();
-                            realm.beginTransaction();
-                            note.deleteFromRealm();
-                            realm.commitTransaction();
-                            Toast.makeText(context, "Note deleted", Toast.LENGTH_SHORT).show();
-                        }
-                        return true;
-                    }
-                });
-                menu.show();
-                return true;
-            }
-        });
+    @Override
+    public int getItemViewType(int position) {
+        return notesList.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_NOTE;
     }
 
     @Override
     public int getItemCount() {
-        return notesList.size();
+        return notesList == null ? 0 : notesList.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    private void showLoadingView(LoadingViewHolder holder, int position) {
+
+    }
+
+    private void populateItemRows(ItemViewHolder holder, int position) {
+        Note note = notesList.get(position);
+        holder.descriptionOutput.setText(note.getDescription());
+    }
 
 
-        TextView descriptionOutput;
+    private class ItemViewHolder extends RecyclerView.ViewHolder {
+        private TextView descriptionOutput;
 
-        public MyViewHolder(@NonNull View itemView) {
+        public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
             descriptionOutput = itemView.findViewById(R.id.descriptionoutput);
         }
+
+//        public void setItem(String item) {
+//            descriptionOutput.setText(item);
+//        }
     }
+
+    private class LoadingViewHolder extends RecyclerView.ViewHolder {
+        private ProgressBar progressBar;
+
+        public LoadingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progressBar);
+        }
+    }
+
 }
--- app/src/main/java/com/example/mapmopad/Search.java
+++ app/src/main/java/com/example/mapmopad/Search.java
@@ -1,24 +1,42 @@
 package com.example.mapmopad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
 
 import io.realm.Realm;
 import io.realm.RealmChangeListener;
 import io.realm.RealmResults;
 
 public class Search extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MyAdapter adapter;
+    RealmResults<Note> notesList;
+
+    private boolean isLoading = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Button homebtn = (Button) findViewById(R.id.home);
+        homebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Button addmemobtn = (Button) findViewById(R.id.addmemo2);
         addmemobtn.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +63,52 @@ public class Search extends AppCompatActivity {
                 myAdapter.notifyDataSetChanged();
             }
         });
+
+//        initScrollListener();
     }
+
+    private void initScrollListener() {
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+
+                if (!isLoading) {
+                    if (layoutManager != null && layoutManager.findLastCompletelyVisibleItemPosition() == notesList.size() - 1) {
+                        //리스트 마지막
+                        loadMore();
+                        isLoading = true;
+                    }
+                }
+            }
+        });
+    }
+
+    private void loadMore() {
+        notesList.add(null);
+        adapter.notifyItemInserted(notesList.size() - 1);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                notesList.remove(notesList.size() - 1);
+                int scrollPosition = notesList.size();
+                adapter.notifyItemRemoved(scrollPosition);
+                int currentSize = scrollPosition;
+                int nextLimit = currentSize + 10;
+
+                adapter.notifyDataSetChanged();
+                isLoading = false;
+            }
+        }, 2000);
+    }
+
 }
