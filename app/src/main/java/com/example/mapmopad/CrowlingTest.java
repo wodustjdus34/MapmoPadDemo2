6b39f5ab57dd0ded983ded24b052a7b4d2452b47
wodus <wodustjdus34@gmail.com>
2022-08-10 16:20:23 +0900

    웹 크롤링 코드 수정 (작동 X: 동적 웹 페이지)

--- app/src/main/java/com/example/mapmopad/CrowlingTest.java
+++ app/src/main/java/com/example/mapmopad/CrowlingTest.java
@@ -18,6 +18,8 @@ import java.io.IOException;
 import java.text.BreakIterator;
 
 public class CrowlingTest extends AppCompatActivity {
+    String keyword;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,43 +29,34 @@ public class CrowlingTest extends AppCompatActivity {
         final Bundle bundle = new Bundle();
 
         String URL = "https://m.some.co.kr/analysis/social/association?keyword=%EC%8A%A4%EC%BF%BC%ED%8A%B8&startDate=20220709&endDate=20220808&sources=blog%2cnews%2ctwitter";
-        boolean isEmpty;
 
         new Thread() {
             @Override
             public void run() {
+                Document doc = null;
                 try {
-                    String keyword;
                     //크롤링 할 구문
-                    Document doc = Jsoup.connect(URL).get();
-                    Elements temele = doc.select("analysis-desc-card-item analysis-desc-card-item-date");
-                    boolean isEmpty = temele.isEmpty();
-                    if (isEmpty == false) { //null값이 아니면 크롤링 실행
-                        keyword = temele.get(0).text(); //크롤링 하면 "현재온도30'c" 이런식으로 뽑아와지기 때문에, 현재온도를 잘라내고 30'c만 뽑아내는 코드
-                        bundle.putString("temperature", keyword);
-                    }
+                    doc = Jsoup.connect(URL).get();
+                    Elements contents = doc.select("#mostRelevantKeyword");
+                    keyword += "keyword: " + contents.text();
+                    bundle.putString("relevantkeyord", keyword);
+                    Message keyword = handler.obtainMessage();
+                    keyword.setData(bundle);
+                    handler.sendMessage(keyword);
+
                 } catch (IOException e) {
                     e.printStackTrace();
-                }
+                    }
             }
         }.start();
-
-
     }
 
-    Handler handler;
-
-    {
-        handler = new Handler(Looper.getMainLooper()) {
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                Bundle bundle = msg.getData();    //new Thread에서 작업한 결과물 받기
-                BreakIterator textView = null;
-                textView.setText(bundle.getString("temperature"));    //받아온 데이터 textView에 출력
-            }
-
-            ;
+    Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message keyword) {
+            Bundle bundle = keyword.getData();    //new Thread에서 작업한 결과물 받기
+            textView.setText(bundle.getString("relevantkeyord"));    //받아온 데이터 textView에 출력
         };
-    }
+    };
 
 }
