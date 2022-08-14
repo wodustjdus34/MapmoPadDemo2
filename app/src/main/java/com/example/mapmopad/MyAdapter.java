eaaa78fb4c74720ac482c76cd2934648b367673d
Juyeon Lee <johndoe@example.com>
2022-08-14 23:41:15 +0900

    [fix] recyclerview 오류 해결

--- app/src/main/java/com/example/mapmopad/MyAdapter.java
+++ app/src/main/java/com/example/mapmopad/MyAdapter.java
@@ -17,56 +17,15 @@ import io.realm.RealmResults;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
-    Context context;
     RealmResults<Note> notesList;
 
-    public MyAdapter(Context context, RealmResults<Note> notesList) {
-        this.context = context;
+
+
+    public MyAdapter(RealmResults<Note> notesList) {
         this.notesList = notesList;
     }
 
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_view, parent, false));
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        Note note = notesList.get(position);
-        holder.descriptionOutput.setText(note.getDescription());
-
-        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-
-                PopupMenu menu = new PopupMenu(context, v);
-                menu.getMenu().add("DELETE");
-                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        if (item.getTitle().equals("DELETE")) {
-                            Realm realm = Realm.getDefaultInstance();
-                            realm.beginTransaction();
-                            note.deleteFromRealm();
-                            realm.commitTransaction();
-                            Toast.makeText(context, "Note deleted", Toast.LENGTH_SHORT).show();
-                        }
-                        return true;
-                    }
-                });
-                menu.show();
-                return true;
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        return notesList.size();
-    }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
 
 
         TextView descriptionOutput;
@@ -76,4 +35,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             descriptionOutput = itemView.findViewById(R.id.descriptionoutput);
         }
     }
+
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.item_view, parent, false);
+        MyAdapter.MyViewHolder vh = new MyAdapter.MyViewHolder(view);
+        return vh;
+
+        }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Note note = notesList.get(position);
+        holder.descriptionOutput.setText(note.getDescription());
+
+        };
+
+
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+
 }
