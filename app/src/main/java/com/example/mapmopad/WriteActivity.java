ee5f7bcf1a830351ad106de9b7e6888bb30b4080
Juyeon Lee <johndoe@example.com>
2022-08-16 21:52:41 +0900

    [feature] wordList 추가 함수

--- app/src/main/java/com/example/mapmopad/WriteActivity.java
+++ app/src/main/java/com/example/mapmopad/WriteActivity.java
@@ -49,6 +49,9 @@ public class WriteActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    // wordList 생성 함수
     // (예 : 학교를 학교를 가기싫다./n수업을 듣기싫다.)
     // 단어 나누기 (예 : {'학교를','학교를','가기싫다.','','수업을''듣기싫다'} )
     public String[] FindKeyword1(String s) {
@@ -58,21 +61,25 @@ public class WriteActivity extends AppCompatActivity {
 
     // 목적어 추출하기 (예 : {'학교','수업'})
     public Vector<String> FindKeyword2(String[] s){
-        Vector<String> v = new Vector<>(); // {'학교','학교','수업'}
+        Vector<String> v = new Vector<>();
 
         for(int i = 0; i< s.length; i++){
-            if ( s[i].endsWith("s")&&!v.contains(s[i].substring(0, s[i].length()-1))){
-                v.add(s[i].substring(0, s[i].length()-1));
+            if(!s[i].equals("")){
+                String sSub = s[i].substring(0,s[i].length()-1); // 맨 뒷 글자를 뺀 단어
+                if ( (s[i].endsWith("을")||s[i].endsWith("를"))&&!v.contains(sSub)){
+                    v.add(sSub);
+                }
             }
         }
         return v;
     }
 
     // 나눈 단어끼리 비교하기
-    public Vector<String> FindKeyword3(String[] s1, String s2) {
-        Vector<String> list = new Vector<>();
-        Vector<String> l = new Vector<>();
-        for (int i = 0; i<s1.length; i++)
-
+    public Vector<String> FindKeyword3(Vector<String> sSub1, Vector<String> sSub2) {
+        Vector<String> compared = new Vector<>();
+        for (int i = 0; i<sSub1.size(); i++){
+            if(sSub2.contains(sSub1.get(i))) compared.add(sSub1.get(i));
+        }
+        return compared;
     }
 }
