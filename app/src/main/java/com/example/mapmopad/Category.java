66a86ec69ab9052766911dce88eba437b90a3aad
Juyeon Lee <johndoe@example.com>
2022-08-17 07:28:17 +0900

    [feature] wordList 멤버변수 제거 및 Keyword 클래스 구현

--- app/src/main/java/com/example/mapmopad/Category.java
+++ app/src/main/java/com/example/mapmopad/Category.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class category extends AppCompatActivity {
+public class Category extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
